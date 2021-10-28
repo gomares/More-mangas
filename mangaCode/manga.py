648abe2bf8ff1348f7d664f28481c4486ddb93ef
Xileon310 <joseluis.alfaix@gmail.com>
2021-10-28 11:27:05 +0200

    Se cambia nombre de variables

--- mangaCode/manga.py
+++ mangaCode/manga.py
@@ -3,7 +3,7 @@ class Manga:
 		self._name = name
 		self._description = description
 		self._genre = genre
-		self._num_capitulos = num_capitulos
+		self._num_chapters = _num_chapters
 		self._mangaka = mangaka
 
 	def get_name(self):
@@ -15,8 +15,8 @@ class Manga:
 	def get_genre(self):
 		return self.genre
 
-	def get_num_capitulos(self):
-		return self._num_capitulos
+	def get_num_chapters(self):
+		return self._num_chapters
 
 	def get_mangaka(self):
 		return self._mangaka

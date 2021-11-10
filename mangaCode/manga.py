d1bdf0069e4d903a7ec054d3f1511d5104c1f956
Xileon310 <joseluis.alfaix@gmail.com>
2021-11-10 13:13:40 +0100

    Elimino atributos que no sirven a la lógica de negocio actualizando #8

--- mangaCode/manga.py
+++ mangaCode/manga.py
@@ -1,25 +1,17 @@
 import array
 
 class Manga:
-	def __init__(self, name, description, genre, num_chapters, mangaka):
+	def __init__(self, name, genre, mangaka):
 		self._name = name
-		self._description = description
 		self._genre = genre
-		self._num_chapters = num_chapters
 		self._mangaka = mangaka
 		self._reviews = []
 
 	def get_name(self):
 		return self._name
 
-	def get_description(self):
-		return self._description
-
 	def get_genre(self):
 		return self._genre
-
-	def get_num_chapters(self):
-		return self._num_chapters
 
 	def get_mangaka(self):
 		return self._mangaka

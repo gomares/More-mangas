2d0b930e8f59633db3bef033e399c5de7aeac405
Xileon310 <joseluis.alfaix@gmail.com>
2021-11-10 13:26:11 +0100

    Clase User y Clase Manga
    Se eliminan los getters y se deja solo los atributos. Closes #8 y closes #10

--- mangaCode/manga.py
+++ mangaCode/manga.py
@@ -6,15 +6,3 @@ class Manga:
 		self._genre = genre
 		self._mangaka = mangaka
 		self._reviews = []
-
-	def get_name(self):
-		return self._name
-
-	def get_genre(self):
-		return self._genre
-
-	def get_mangaka(self):
-		return self._mangaka
-
-	def get_reviews(self):
-		return self._reviews
--- mangaCode/user.py
+++ mangaCode/user.py
@@ -5,19 +5,4 @@ class User:
 		self._surnames = surnames
 		self._email = email
 		self._passwd = passwd
-
-	def get_nickname(self):
-		return self._nickname
-
-	def get_name(self):
-		return self._name
-
-	def get_surnames(self):
-		return self._surnames
-
-	def get_email(self):
-		return self._email
-
-	def get_passwd(self):
-		return self._passwd
 		

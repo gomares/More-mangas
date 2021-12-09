47b7683ef92383fbe52c85420227d3ae00f4868f
gomares <e.gomezacuyo@go.ugr.es>
2021-12-09 22:03:40 +0100

    Corrección de validación de géneros
    Daba un error manejando el enum y ya está solucionado, se ha añadido el atributo de puntuación media que jugará un papel importante en nuestra lógica de negocio.

---
+++ manga/manga.py
@@ -0,0 +1,97 @@
+from errors import GenreNotFoundError, MangakaTypeError
+from genre import Genre
+
+
+
+class Manga:
+    """
+    Entidad que representa un manga.
+
+    Attributes
+    ----------
+    name : str
+            Nombre del manga.
+    genres : list of Genre
+            Lista de géneros a los que pertenece el manga.
+    mangakas : list of string
+            Lista de mangakas que han desarrollado la obra.
+    num_vols : int
+            Número de volúmenes en los que se compone el manga.
+puntuacion_media : float
+    Puntuación media que los usuarios dieron al manga.
+
+    Methods
+    -------
+    get_name()
+            Devuelve el nombre del manga.
+    get_genres()
+            Devuelve una lista con objetos de tipo Genre.
+    get_mangakas()
+            Devuelve una lista de strings con los nombres de los autores.
+    get_num_vols()
+            Devuelve un entero con el número de volúmenes de la obra.
+    get_puntuacion_media()
+            Devuleve un flotante con la puntuación media del manga en cuestión.
+
+    """
+
+    def __init__(self, name: str, genres: list, mangakas: list, num_vols: int, puntuacion_media: float):
+        """
+        Constructor de la entidad
+
+        Parameters
+        ----------
+        name : str
+                Nombre del manga.
+        genres : list of Genre
+                Lista de géneros a los que pertenece el manga.
+        mangakas : list of string
+                Lista de mangakas que han desarrollado la obra.
+        num_vols : int
+                Número de volúmenes en los que se compone el manga.
+        puntuacion_media : float
+        Puntuación media que los usuarios dieron al manga.
+        """
+
+        # Comprobación de errores
+
+        # Validación géneros
+        l = []
+        for genre in Genre:
+            l.append(genre)
+
+        es_correcto = False
+        if set(genres) <= set(l):
+            es_correcto = True
+
+        if es_correcto == False:
+            #raise GenreNotFoundError()
+            pass
+
+        # Validación autores
+        for mangaka in mangakas:
+            if type(mangaka) != str:
+                raise MangakaTypeError()
+
+        # Una vez que los valores son correctos se establecen en la clase.
+        self._name = name
+        self._genres = genres
+        self._mangakas = mangakas
+        self._num_vols = num_vols
+        self._puntuacion_media = puntuacion_media
+
+    def get_name(self):
+        return self._name
+
+    def get_genres(self):
+        return self._genres
+
+    def get_mangakas(self):
+        return self._mangakas
+
+    def get_num_vols(self):
+        return self._num_vols
+
+    def get_puntuacion_media(self):
+        return self._puntuacion_media
+

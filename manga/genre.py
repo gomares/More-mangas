ad29e7d9ccb16fe572cb80600b4d3bc60acfe646
gomares <e.gomezacuyo@go.ugr.es>
2021-12-11 13:49:19 +0100

    Modificación de la clse Genre
    Deja de ser un enumerado y pasa a ser una lista, eso nos ayudará con la validación de datos ya que se hará directamente comparando dos listas y además es más sencillo serializar un alista en JSON.

--- manga/genre.py
+++ manga/genre.py
@@ -1,28 +1,31 @@
-from enum import Enum, auto
 
-class Genre(Enum):
+
+class Genre:
 	"""
 
-	Objeto valor que representa el género de un manga
+    Entidad que representa géneros de un manga.
+
+    Attributes
+    ----------
+    name : str
+        Nombre del manga.
+    generos : list of String
+        Lista de géneros que puede tener el manga.
+
+
 
 	"""
-	SHONEN = auto()
-	SEINEN = auto()
-	SHOJO = auto()
-	JOSEI = auto()
-	KODOMO = auto()
-	ISEKAI = auto()
-	CYBERPUNK = auto()
-	YAOI = auto()
-	YURI = auto()
-	ECCHI = auto()
-	HENTAI = auto()
-	SPOKON = auto()
-	MAGICAL_GIRLS = auto()
-	MECHAS = auto()
+
+	def __init__(self) -> None:
+		"""
+        	Constructor de la entidad
+		"""
+	
+		self.generos=["SHONEN","SEINEN","SHOJO","JOSEI","KODOMO","ISEKAI","CYBERPUNK","YAOI","YURI","ECCHI","HENTAI","SPOKON","MAGICAL_GIRLS","MECHAS"]
+
 
 
 # Ponderación de los géneros
-ponderacion = {Genre.SHONEN: 7, Genre.SEINEN: 5, Genre.SHOJO: 7, Genre.JOSEI: 10, Genre.KODOMO: 4, Genre.ISEKAI: 7,
-               Genre.CYBERPUNK: 8, Genre.YAOI: 9, Genre.YURI: 9, Genre.ECCHI: 9, Genre.HENTAI: 10, Genre.SPOKON: 3,
-               Genre.MAGICAL_GIRLS: 6, Genre.MECHAS: 6}
+ponderacion = {"SHONEN": 7, "SEINEN": 5, "SHOJO": 7, "JOSEI": 10, "KODOMO": 4, "ISEKAI": 7,
+               "CYBERPUNK": 8, "YAOI": 9, "YURI": 9, "ECCHI": 9, "HENTAI": 10, "SPOKON": 3,
+               "MAGICAL_GIRLS": 6, "MECHAS": 6}
